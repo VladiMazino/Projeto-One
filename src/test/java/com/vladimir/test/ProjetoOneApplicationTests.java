e2e1caeb06d317fdcee9030c3fed2349d12dcac3
VladiNicolau <vladimir.nicolau45@gmail.com>
2023-10-25 20:56:01 -0300

    Versão inicial do projeto

---
+++ src/test/java/com/vladimir/test/ProjetoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimir.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
